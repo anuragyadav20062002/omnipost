5e610a307d64e4a0348ad7e275781516e2c92ef9
Anurag <anuragyadav20062002@gmail.com>
2025-01-31 19:54:16 +0530

    sjd

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -2,7 +2,7 @@ import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
-export async function GET(req: Request) {
+export async function GET() {
   const cookieStore = cookies()
   const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
 
